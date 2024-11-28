497ca1f88e25330be486162a0db5f1eaaf5ae053
portillojesus <jesusportillo000018@gmail.com>
2024-11-28 00:35:28 -0300

    f

--- python_vercel_task/settings.py
+++ python_vercel_task/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.application']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.application', 'Supabase-y-Vercel.vercel.app']
 
 
 
